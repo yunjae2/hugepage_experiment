2c4303c5bb624a3cce7047be308c11388fa36447
Yunjae Lee <lyj7694@gmail.com>
2019-09-17 16:12:49 +0900

    Pollute tlb before accessing main object
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- measure.c
+++ measure.c
@@ -3,6 +3,9 @@
 #include <string.h>
 
 #define BASEPAGE_SIZE	4096
+#define HUGEPAGE_SIZE	(2 * 1024 * 1024)
+#define TLB_SIZE	(512 * 4 * 1024)	// 512 entries with 4K
+#define HUGETLB_SIZE	(32 * 2 * 1024 * 1024)	// 32 entries with 2M
 
 int *object;
 
@@ -18,12 +21,34 @@ void init_object(size_t size)
 		printf("Object allocation failed!\n");
 		exit(1);
 	}
+
+	memset(object, 0, size);
 }
 
 void madvise_object(size_t size, int madvise)
 {
 }
 
+void pollute_tlb(int huge)
+{
+	char *pollute_data;
+	int tlb_size, page_size;
+	int i;
+
+	if (huge) {
+		tlb_size = HUGETLB_SIZE;
+		page_size = HUGEPAGE_SIZE;
+	} else {
+		tlb_size = TLB_SIZE;
+		page_size = BASEPAGE_SIZE;
+	}
+	posix_memalign((void **)&pollute_data, page_size, tlb_size);
+
+	for (i = 0; i < tlb_size; i++)
+		pollute_data[i] = (char) i;
+
+	free(pollute_data);
+}
 void access_object(size_t size)
 {
 }
@@ -54,6 +79,7 @@ int main(int argc, char **argv)
 
 	init_object(size);
 	madvise_object(size, madvise_huge);
+	pollute_tlb(madvise_huge);
 	access_object(size);
 	free_object();
 

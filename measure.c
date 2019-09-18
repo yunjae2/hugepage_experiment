9a05520d7848babf46de8694e242637d5834929e
Yunjae Lee <lyj7694@gmail.com>
2019-09-18 13:00:31 +0900

    Implement monitoring of tlb misses
    Only read misses to dtlb is accounted.
    
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- measure.c
+++ measure.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <linux/perf_event.h>
+#include <unistd.h>
+#include <asm/unistd.h>
 
 #define BASEPAGE_SIZE	4096
 #define HUGEPAGE_SIZE	(2 * 1024 * 1024)
@@ -9,12 +13,23 @@
 #define HUGETLB_SIZE	(32 * 2 * 1024 * 1024)	// 32 entries with 2M
 
 int *object;
+int perf_fd;
 
 /*
  * We have two type of options;
  * 1. Use huge page?
  * 2. Size of object
  */
+
+long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+		int cpu, int group_fd, unsigned long flags)
+{
+	int ret;
+
+	ret = syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd,
+			flags);
+	return ret;
+}
 
 void init_object(size_t size)
 {
@@ -61,6 +76,44 @@ void pollute_tlb(int huge)
 	free(pollute_data);
 }
 
+void perf_record(void)
+{
+	struct perf_event_attr pe;
+
+	__u64 perf_hw_cache_id = PERF_COUNT_HW_CACHE_DTLB;
+	__u64 perf_hw_cache_op_id = PERF_COUNT_HW_CACHE_OP_READ;
+	__u64 perf_hw_cache_op_result_id = PERF_COUNT_HW_CACHE_RESULT_MISS;
+
+	memset(&pe, 0, sizeof(struct perf_event_attr));
+	pe.type = PERF_TYPE_HW_CACHE;
+	pe.size = sizeof(struct perf_event_attr);
+	pe.disabled = 1;
+	pe.exclude_hv = 1;
+	pe.config = (perf_hw_cache_id) | (perf_hw_cache_op_id << 8) |
+		(perf_hw_cache_op_result_id << 16);
+
+	perf_fd = perf_event_open(&pe, 0, 0, -1, 0);
+	if (perf_fd == -1) {
+		fprintf(stderr, "Error opening leader %llx\n", pe.config);
+		exit(EXIT_FAILURE);
+	}
+
+	ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
+	ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
+}
+
+void perf_report(void)
+{
+	long long count;
+
+	ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, 0);
+	read(perf_fd, &count, sizeof(long long));
+
+	printf("dtlb misses: %lld\n", count);
+
+	close(perf_fd);
+}
+
 void access_object(size_t size)
 {
 	int i;
@@ -97,7 +150,9 @@ int main(int argc, char **argv)
 	init_object(size);
 	madvise_object(size, madvise_huge);
 	pollute_tlb(madvise_huge);
+	perf_record();
 	access_object(size);
+	perf_report();
 	free_object();
 
 	return 0;
